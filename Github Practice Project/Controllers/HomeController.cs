ba13ab2d75ebf4f88b3fcb3371c401822c4d04b1
Al-Sharif <phsarif88@gmail.com>
2024-07-14 11:42:16 +0600

    Home Controller Added

---
+++ Github Practice Project/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Github_Practice_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
